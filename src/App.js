f936e234346446ac4d9e217685bb72311896482c
Nathanael <nathanaelniyogushimwa@gmail.com>
2024-01-04 00:26:00 -0800

    project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Home } from "./pages/Home";
+import { Aboutus } from "./pages/Aboutus";
+import { Service } from "./pages/Service";
+import { Contact } from './pages/Contact';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>hello react user???</h1>
     </div>
+
   );
 }
 
